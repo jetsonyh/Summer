5ae552ed4ccd69723080bacafbe872ebd7533daf
yao hao <yh403666@126.com>
2015-06-01 19:22:09 +0800

    Initial Commit

---
+++ Summer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Summer
+//
+//  Created by ly on 15/6/1.
+//  Copyright (c) 2015年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Summer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Summer
+//
+//  Created by ly on 15/6/1.
+//  Copyright (c) 2015年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
